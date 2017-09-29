3b722ab7029db1f2b5399be8f794f8f55e82056e
Zachery Moneypenny <whazzmaster@gmail.com>
2017-09-29 14:55:44 -0500

    Fixes specs

--- spec/cryptid/client_spec.rb
+++ spec/cryptid/client_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 require 'excon'
 
 RSpec.describe Cryptid::Client do
+  let(:tracker_id) { 'web|abc123' }
   let(:url) { Cryptid::Configuration.new.url }
   let(:excon) { Excon.new(url, mock: true) }
-  let(:client) { described_class.new(conn: excon) }
+  let(:client) { described_class.new(conn: excon, tracker_id: tracker_id) }
 
   describe '#url' do
     it 'defaults to the production url' do
@@ -14,7 +15,12 @@ RSpec.describe Cryptid::Client do
 
   describe '#send' do
     let(:expected_body) do
-      {event: {eventValue: 'login-successful'}}.to_json
+      {
+        event: {
+          eventValue: 'login-successful',
+          trackerId: tracker_id,
+        }
+      }.to_json
     end
 
     it 'sends the event in an envelope in the body' do
